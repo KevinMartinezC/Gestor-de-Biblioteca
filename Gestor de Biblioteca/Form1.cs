da8ea3c8886376465496589573a29fea42fadfe7
KevinMartinezC <ceronk003@gmail.com>
2025-10-05 22:35:23 -0600

    Agregar archivos de proyecto.

---
+++ Gestor de Biblioteca/Form1.cs
@@ -0,0 +1,15 @@
+namespace Gestor_de_Biblioteca
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
